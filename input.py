c15d6dfe919fdc98f17309e28315889a2fc4bd64
pravinkalale <kalalepravin2@gmail.com>
2026-09-21 19:17:42 +0530

    Add first input program

---
+++ input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("Enter your city: ")
+
+print("My name is",name)
+print("I am", age, "Years old")
+print("I live in", city)
