06393e3021f24d78affe36c32b44c7a40b7127bd
pravinkalale <kalalepravin2@gmail.com>
2026-09-24 22:37:56 +0530

    Add Operators programs

---
+++ Optr.py
@@ -0,0 +1,82 @@
+a =10
+b =2
+# result = a*b
+
+print("addition: ",a+b)
+print("sub: ",a-b)
+print("Multi: ",a*b)
+print("expon : ",a**b)
+print("division : ",a/b)
+print("Reminder : ",a%b)
+print("floor div : ",a//b)
+
+
+# Relational operators
+
+x=10
+y=5
+print("Equals to optr :", x==y)
+print("less than :", x<y)
+print("greater than :", x>y)
+print("less than equal to  :", x<=y)
+print("greater than equal to  :", x<=y)
+print("Not  equal to  :", x!=y)
+
+# Logical Operators
+
+a =10>5 and 10>8 and 10<6
+print(a)
+
+print(10 and 20)
+
+print(-10 or -20)
+
+print(" " or "Hi")
+
+print("True" and "False")
+
+print("-3" and 0)
+
+print(" " or 12)
+
+print("str" or "strl")
+
+print("Hello" and "Hello jii")
+
+print(True or False)
+
+print( True and False)
+
+
+# Assignment Operator/ shorthand optr
+
+k = 10
+L = 6
+
+k = L
+print(k)      # assign
+
+k += L
+print(k)      # add and assign
+
+k -= L
+print(k)      # subtract and assign
+
+k /= L
+print(k)      # divide and assign
+
+k *= L
+print(k)      # multiply and assign
+
+k %= L
+print(k)      # modulus and assign
+
+k **= L
+print(k)      # exponent and assign
+
+k //= L
+print(k)      # floor division and assign
+
+
+
+
